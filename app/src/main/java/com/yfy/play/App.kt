a3b158998ad2275829337c9659ca30f9c53f4f6f
yfy <1639882927@qq.com>
2023-12-22 15:49:53 +0800

    App里监听gc发生时间

--- app/src/main/java/com/yfy/play/App.kt
+++ app/src/main/java/com/yfy/play/App.kt
@@ -26,6 +26,7 @@ class App : Application() {
         super.onCreate()
         Play.initialize(this)
         Util.init(this)
+        initGcWatcher()
         initData()
     }
 
--- core/src/main/java/com/yfy/core/util/UiUtils.kt
+++ core/src/main/java/com/yfy/core/util/UiUtils.kt
@@ -1,7 +1,9 @@
 package com.yfy.core.util
 
 import android.content.Context
+import android.os.SystemClock
 import android.view.View
+import java.lang.ref.WeakReference
 
 /**
  * dip转换成pixel
@@ -21,5 +23,32 @@ inline fun View.setSafeListener(crossinline action: () -> Unit) {
         if (gap < 800) return@setOnClickListener
         action.invoke()
         lastClick = System.currentTimeMillis()
+    }
+}
+
+private var sGcWatcher: WeakReference<GcWatcherInternal>? = null
+
+/**
+ * 监听gc发生
+ */
+fun initGcWatcher() {
+    if (sGcWatcher == null) {
+        LogUtil.i("UiUtils", "GcWatcher is init")
+        sGcWatcher = WeakReference(GcWatcherInternal())
+    } else {
+        LogUtil.e("UiUtils", "GcWatcher is already init,don't need init again")
+    }
+}
+
+private var sLastGcTime = 0L
+
+private class GcWatcherInternal {
+    @kotlin.jvm.Throws(Throwable::class)
+    protected fun finalize() {
+        val now = SystemClock.uptimeMillis()
+        val happenDuration = now - sLastGcTime
+        LogUtil.i("UiUtils", "happened gc!!！！！！！！!$now - $sLastGcTime = $happenDuration ms interval from last time")
+        sLastGcTime = now
+        sGcWatcher = WeakReference(GcWatcherInternal())
     }
 }
